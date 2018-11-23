f6a04ec95cff900f90bf00d6f5de130227987da4
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-11-23 15:23:09 -0500

    pantalla1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
     <v-content id="back">
-      <router-view></router-view>
+      <router-view class="v-align-items"></router-view>
+
     </v-content>
   </v-app>
 </template>
@@ -41,8 +42,24 @@
   }
 </script>
 <style media="screen">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
   #back {
     width: 100vh;
     background-image: url('https://lh4.googleusercontent.com/BUecxAUQHxbQEDyUrPxazu5vHfosaGvAuuWtD_Kb_BU6aPGju5KOakYQDzDagDgWBvzVCR51zKo3DxK0zFxq=w1366-h657-rw');
+  }
+  #bvl-logo {
+    background-color: white;
+    border-radius: 70px 0 0 0 ;
+    margin-left: 68%;
+    margin-top: 37%;
+  }
+  #bvl-png {
+    width: 110px;
+    margin-left: 10%;
+    padding-right: 15%;
+    padding-left: 10%;
+    padding-top: 10%;
+    padding-bottom: 10%;
   }
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,17 +2,19 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs12 text-xs-center mt-5>
-        <img src="https://lh5.googleusercontent.com/F30AFK_q3rQVRQ1nsjCwt5kr5qZ_e1sIkOgQGiM3wfy9okHkpDNTvS15W2SFLqFp9mD0-Iy68VGeODOkLsUn=w1366-h657" alt="">
+        <img id="logo" src="https://lh5.googleusercontent.com/F30AFK_q3rQVRQ1nsjCwt5kr5qZ_e1sIkOgQGiM3wfy9okHkpDNTvS15W2SFLqFp9mD0-Iy68VGeODOkLsUn=w1366-h657" alt="">
       </v-flex>
-      <v-flex xs12 sm6 offset-sm3 mt-3>
-        <blockquote class="blockquote text-xs-center">
-          Aprende a invetir en la bolsa <br>
-          ...como jugando!!!
+      <v-flex>
+        <blockquote class="texto-white blockquote text-xs-center">
+          <strong>Ingresa con</strong>
         </blockquote>
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3 class="text-xs-center" mt-5>
         <v-btn color="primary" to="/signup">Sign Up</v-btn>
         <v-btn to="/signin">Sign In</v-btn>
+      </v-flex>
+      <v-flex id="bvl-logo">
+        <img id="bvl-png" src="http://subirimagen.me/uploads/20181123120801.png" alt="">
       </v-flex>
     </v-layout>
   </v-container>
@@ -21,3 +23,15 @@
 <script>
 export default {}
 </script>
+
+<style media="screen">
+  #logo {
+    width: 75%;
+    margin-bottom: 40px;
+  }
+  .texto-white {
+    color: white;
+    font-size: 26px;
+    font-weight: 390;
+  }
+</style>
