e7946ad7ea47d37b5cd157cb3261f6382c643653
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-11-23 12:24:58 -0500

    fondo e imagen

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sidebar" app>
-      <v-list>
-        <v-list-tile
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="userSignOut" v-if="isAuthenticated">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>Sign Out</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-toolbar app>
-      <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar">
-        </v-toolbar-side-icon>
-      </span>
-      <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">
-          {{ appTitle }}
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-        <v-btn flat @click="userSignOut" v-if="isAuthenticated">
-          <v-icon left>exit_to_app</v-icon>
-          Sign Out
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    
-    <v-content>
+    <v-content id="back">
       <router-view></router-view>
     </v-content>
-    
   </v-app>
 </template>
 
@@ -58,13 +10,7 @@
   export default {
     data () {
       return {
-        // appTitle: 'Awesome App',
         sidebar: false
-        // menuItems: [
-        //   { title: 'Home', path: '/home', icon: 'home' },
-        //   { title: 'Sign Up', path: '/signup', icon: 'face' },
-        //   { title: 'Sign In', path: '/signin', icon: 'lock_open' }
-        // ]
       }
     },
     computed: {
@@ -94,3 +40,9 @@
     }
   }
 </script>
+<style media="screen">
+  #back {
+    width: 100vh;
+    background-image: url('https://lh4.googleusercontent.com/BUecxAUQHxbQEDyUrPxazu5vHfosaGvAuuWtD_Kb_BU6aPGju5KOakYQDzDagDgWBvzVCR51zKo3DxK0zFxq=w1366-h657-rw');
+  }
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs12 text-xs-center mt-5>
-        <h1>Bienvenido a Stock Evolution</h1>
+        <img src="https://lh5.googleusercontent.com/F30AFK_q3rQVRQ1nsjCwt5kr5qZ_e1sIkOgQGiM3wfy9okHkpDNTvS15W2SFLqFp9mD0-Iy68VGeODOkLsUn=w1366-h657" alt="">
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3>
         <blockquote class="blockquote text-xs-center">
