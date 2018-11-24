696264ecfde762d4b1b2d8b4e9e235e8343eabf6
noelyGitHub <noely.alis209@gmail.com>
2018-11-24 10:19:12 -0600

    create data leo

---
+++ src/plugins/data_leo.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const intro = [
+  {
+    src: 'http://subirimagen.me/uploads/20181123161541.png',
+    message: '!Hola! soy Leo y te voy a guiar a lo largo del juego.'
+  },
+  {
+    src: 'http://subirimagen.me/uploads/20181123161725.png',
+		message: 'Todos aquí iniciammos nuestro camino a ser unos gigantes de la inversión con $5000'
+	},
+  {
+	  src: 'http://subirimagen.me/uploads/20181123161838.gif',
+    message: 'Conviértete en una grande de la inversión, siguiendo mis consejos.'
+  }
+]
+export default{intro}
