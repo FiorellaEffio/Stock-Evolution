6089f87a00bde7c99dde28a87a4280937000a87e
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-11-30 11:11:03 -0500

    quitando doble cerrar sesion

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,49 +1,18 @@
 <template>
  <div id="app">
  <nav class="navTool">
-   <!-- <v-toolbar dark >
-
-     <v-spacer></v-spacer>
-     <v-toolbar-items color="primary" >
-       <v-btn flat="flat">
-
-       </v-btn>
-
-     </v-toolbar-items>
-   </v-toolbar> -->
    <div class="hidden-md-and-up"  >
      <v-expansion-panel class="backExpans">
        <v-expansion-panel-content class="indigo darken-4">
          <div slot="header">
            <v-toolbar-title class="nameNav">juanita   s/.50000   nivel 1</v-toolbar-title>
          </div>
-         <!-- <v-card>
-           <router-link class="nav-item" to="/about">
-             <v-card-text>About</v-card-text>
-           </router-link>
-         </v-card> -->
-
            <router-link class="nav-item" to="/Landing">
-
                <a @click="userSignOut">
-            <p class="text"><img src="http://subirimagen.me/uploads/20181130025019.png
-http://subirimagen.me/uploads/20181130025019.png
-" width="20px">
+            <p class="text"><img src="http://subirimagen.me/uploads/20181130025019.png" width="20px">
             Cerrar sesión</p>
         </a>
-
-           </router-link>
-            <router-link class="nav-item" to="/Landing">
-
-               <a @click="userSignOut">
-            <p class="text"><img src="http://subirimagen.me/uploads/20181130025019.png
-http://subirimagen.me/uploads/20181130025019.png
-" width="20px">
-            Cerrar sesión</p>
-        </a>
-
-           </router-link>
-
+         </router-link>
        </v-expansion-panel-content>
      </v-expansion-panel>
    </div>
