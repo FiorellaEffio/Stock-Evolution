f1e166360b6b0d6d2e7a51bd7ec1b5423d034ebc
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-11-30 13:46:36 -0500

    primera pregunta lista

--- src/components/level-two-questions.vue
+++ src/components/level-two-questions.vue
@@ -3,18 +3,9 @@
         <v-card-title class="headline">¿Recuerdas que es una SAB?</v-card-title>
         <v-card-text>Una sociedad Agente de Bolsa (SAB) es la única _________ financiera _______________ del mercado bursátil que esta autorizada a la compra y venta que los ____________ solicitan; a cambio cobran una comisión, que es determinada libremente por cada una de ellas.</v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-radio-group v-model="radioGroup">
-            <v-radio
-                v-for="(item, n) in radio"
-                :key="n"
-                :label="item"
-                :value="item"
-            >
-						</v-radio>
-          </v-radio-group>
-					<v-spacer></v-spacer>
-				
+          <v-flex v-for="(item, index) in radio">
+            <input type="radio" class="inlineinput" name="tr" :value="index" v-model="answer">{{item}}<hr>
+          </v-flex>
         </v-card-actions>
         	<v-btn
         class="btn-leo"
@@ -33,13 +24,18 @@ export default {
   data(){
       return {
 				radioGroup: 1,
+        selected: false,
+        answer: 0,
 				radio: ['entidad/intermediaria/inversionista', 'intermediaria/intermediaria/señores', 'entidad/acompañada/bancos']
 			}
   },
   methods: {
 		nextleo(){
-			console.log('envie leo')
-			EventBus.$emit('change-leoTwo_2', true)
+      console.log(this.answer)
+      if(this.answer === 0) {
+        console.log('envie leo')
+        EventBus.$emit('change-leoTwo_2', true)
+      }
 		}
   }
 }
@@ -47,4 +43,3 @@ export default {
 <style scoped>
 
 </style>
-
