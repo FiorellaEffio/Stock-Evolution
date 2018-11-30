18f3394957e269ca4b28f564c7ef495aa902574c
ELEUTERIA <julia.sucasacai@gmail.com>
2018-11-30 15:29:01 -0600

    factores

--- src/components/Sectores.vue
+++ src/components/Sectores.vue
@@ -1,81 +1,85 @@
 <template>
-    <v-container class="text-xs-center">
-        <v-flex mt-4>
-            <h2>¿Qué factores debes tener en cuenta al momento de invertir?</h2>
-        </v-flex>
-        <v-flex mt-4>
-          <v-btn flat large @click="changeFactor(0)"
-                     slot="activator"
-             class="btnEco">
-              <div>
-                  <h4>Sectores, empresas y otros</h4>
-              </div>
-          </v-btn>
-          <v-btn flat large @click="changeFactor(1)"
-             slot="activator"
-             class="btnEco">
-              <div>
-                  <h4>Mercado y recomendaciones</h4>
-              </div>
-          </v-btn>
-          <v-btn  flat large @click="changeFactor(2)"
-                     slot="activator"
-             class="btnEco">
-              <div>
-                  <h4>Mercado local, sectores y empresas</h4>
-              </div>
-          </v-btn>
-        </v-flex>
-        <v-flex mt-4>
-          <v-btn
-          :disabled="!btnActivator"
-         class="btn-Eco"
-           color="red"
-           dark
-         @click="nextLevelTwo()"
-         >
-           Continue
-         </v-btn>
-        </v-flex>
-    </v-container>
+  <v-container class="text-xs-center">
+    <v-flex mt-4>
+      <h2>¿Qué factores debes tener en
+        <br>cuenta al momento de
+        <br>invertir?
+      </h2>
+    </v-flex>
+    <v-flex mt-5>
+      <v-btn flat large @click="changeFactor(0)" slot="activator" class="btnEco">
+        <div>
+          <h4>Sectores, empresas
+            <br>y otros
+          </h4>
+        </div>
+      </v-btn>
+      <v-btn flat large @click="changeFactor(1)" slot="activator" class="btnEco">
+        <div>
+          <h4>Mercado y recomendaciones</h4>
+        </div>
+      </v-btn>
+      <v-btn flat large @click="changeFactor(2)" slot="activator" class="btnEco">
+        <div>
+          <h4>Mercado local, sectores
+            <br>y empresas
+          </h4>
+        </div>
+      </v-btn>
+    </v-flex>
+    <div>
+      <v-btn :disabled="!btnActivator" color="red" @click="nextLevelTwo()" class="btnSec">Continue</v-btn>
+    </div>
+  </v-container>
 </template>
 <script>
-import {EventBus} from '@/plugins/EventBus.js'
- export default {
+import { EventBus } from "@/plugins/EventBus.js";
+export default {
   name: "Inform",
   data() {
-      return {
-        btnActivator: false,
-        factorOption: '',
-      }
+    return {
+      btnActivator: false,
+      factorOption: ""
+    };
   },
   methods: {
-      nextLevelTwo(){
-        if(this.factorOption === 2) {
-          EventBus.$emit('change-leoTwo_5', true)
-        } else {
-          console.log('te equivocaste')
-        }
-      },
-      changeFactor(factorOption) {
-        this.factorOption = factorOption;
-        this.btnActivator = true;
-        console.log(this.factorOption)
+    nextLevelTwo() {
+      if (this.factorOption === 2) {
+        EventBus.$emit("change-leoTwo_5", true);
+      } else {
+        console.log("te equivocaste");
       }
+    },
+    changeFactor(factorOption) {
+      this.factorOption = factorOption;
+      this.btnActivator = true;
+      console.log(this.factorOption);
+    }
   }
 };
 </script>
 <style>
+.btnSec {
+  border-radius: 0.8em;
+  width: 225px;
+  height: 40px;
+}
+
+.textBtn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
 .btnEco {
-  width: 80%;
-  padding: 12%;
-  border: 3px solid #1B2A6F;
- border-radius: 10px
-
+  width: 270px;
+  height: 75px;
+  padding: 5px;
+  border: 2px solid #1b2a6f;
+  border-radius: 8rem;
 }
-.btn-Eco{
-      width: 100%;
-      border-radius: 8rem
-    }
+.btn-Eco {
+  width: 100%;
+  border-radius: 8rem;
+}
 </style>
